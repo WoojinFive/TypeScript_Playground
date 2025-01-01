ee07be7769f18d9817a32ba9e9f597d1f59a6db9
woojinfive <woojin.oh@saltwire.com>
2024-12-31 22:27:40 -0400

    DefinitelyTyped

--- typechain/build/myPackage.js
+++ typechain/build/myPackage.js
@@ -1,20 +1,19 @@
 // @ts-check
 /**
  * Initializes the project
- * @param {object} config 
+ * @param {object} config
  * @param {boolean} config.debug
  * @param {string} config.url
  * @returns boolean
  */
 export function init(config) {
-  return true;
+    return true;
 }
-
 /**
  * Exits the program
- * @param {number} code 
+ * @param {number} code
  * @returns number
  */
 export function exit(code) {
-  return code + 1;
+    return code + 1;
 }
