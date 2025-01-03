4c7bffdbc69f1c5ebab1994da3c5db0c2d907e5a
woojinfive <woojin.oh@saltwire.com>
2025-01-02 21:30:19 -0400

    enum type

--- max_typescript/app.js
+++ max_typescript/app.js
@@ -1,10 +1,33 @@
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]
+// } = {
+//   name: 'aaa',
+//   age: 30,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author']
+// };
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
 var person = {
     name: 'aaa',
     age: 30,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
+    role: Role.READ_ONLY
 };
-person.role.push('admin');
+console.log(Role.READ_ONLY);
+// person.role.push('admin');
+// person.role[1] = 10;
 console.log(person.role);
 var favoriteActivities;
 favoriteActivities = ['Sports'];
@@ -12,4 +35,7 @@ console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+}
+if (person.role === Role.READ_ONLY) {
+    console.log('is read only');
 }
--- max_typescript/app.ts
+++ max_typescript/app.ts
@@ -1,13 +1,30 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string]
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]
+// } = {
+//   name: 'aaa',
+//   age: 30,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author']
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+  ADMIN = 5,
+  READ_ONLY = 100,
+  AUTHOR = 200
+};
+
+const person = {
   name: 'aaa',
   age: 30,
   hobbies: ['Sports', 'Cooking'],
-  role: [2, 'author']
+  role: Role.READ_ONLY
 };
 
 // person.role.push('admin');
@@ -22,4 +39,8 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.READ_ONLY) {
+  console.log('is read only');
 }
