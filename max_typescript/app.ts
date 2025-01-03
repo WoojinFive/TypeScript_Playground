61f752e62c933406f9297e71b6f8e75ff6196139
woojinfive <woojin.oh@saltwire.com>
2025-01-02 21:12:38 -0400

    object, array type

--- max_typescript/app.ts
+++ max_typescript/app.ts
@@ -1,16 +1,19 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return result;
-  }
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[]
+// } = {
+const person = {
+  name: 'aaa',
+  age: 30,
+  hobbies: ['Sports', 'Cooking']
+};
+
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
 }
-
-let number1: number;
-number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is: ';
-
-add(number1, number2, printResult, resultPhrase);
