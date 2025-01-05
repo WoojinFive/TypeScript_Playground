5e52c725c72ee8bca3d786bec5d12bb8685e66ba
woojinfive <kh3996@gmail.com>
2025-01-04 20:42:42 -0400

    include and exclude

--- max_typescript/app.js
+++ max_typescript/app.js
@@ -1,13 +1,3 @@
-var userInput;
-var userName;
-userInput = 5;
-userInput = 'xxx';
-// userName = userInput;
-if (typeof userInput === 'string') {
-    userName = userInput;
-}
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
-}
-var result = generateError('An error occurred!', 500);
-console.log('HERE: ', result);
+"use strict";
+const userName = 'aaa';
+console.log(userName);
--- max_typescript/app.ts
+++ max_typescript/app.ts
@@ -1,18 +1,3 @@
-let userInput: unknown;
-let userName: string;
+const userName = 'aaa';
 
-userInput = 5;
-userInput = 'xxx';
-
-// userName = userInput;
-
-if (typeof userInput === 'string') {
-  userName = userInput;
-}
-
-function generateError(message: string, code: number): never {
-  throw { message: message, errorCode: code };
-}
-
-const result = generateError('An error occurred!', 500);
-console.log('HERE: ', result);
+console.log(userName);
