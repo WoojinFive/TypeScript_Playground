092c4697141c5d66e75c20c71bc77658829e5f93
woojinfive <kh3996@gmail.com>
2025-01-06 14:06:16 -0400

    access modifiers

--- max_typescript/dist/app.js
+++ max_typescript/dist/app.js
@@ -1,14 +1,25 @@
 "use strict";
 class Department {
     constructor(n) {
+        this.employees = [];
         this.name = n;
     }
     describe() {
         console.log('Department: ' + this.name);
     }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 const accounting = new Department('Accounting');
-console.log(accounting);
+accounting.addEmployee('Tom');
+accounting.addEmployee('Janny');
+// accounting.employees[2] = 'Anna';
 accounting.describe();
-const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
-accountingCopy.describe();
+accounting.printEmployeeInformation();
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+// accountingCopy.describe();
