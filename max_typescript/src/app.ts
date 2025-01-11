db8bcea94cbf469a37f4e30232e0ebfcd0ae1b39
woojinfive <kh3996@gmail.com>
2025-01-10 23:17:56 -0400

    index properties

--- max_typescript/src/app.ts
+++ max_typescript/src/app.ts
@@ -110,3 +110,12 @@ const userInputElement = document.getElementById('user-input');
 if (userInputElement) {
   (userInputElement as HTMLInputElement).value = 'Hi there!';
 }
+
+interface ErrorContainer { // { email: 'Not a valid email', username: 'Must start withe a capital character!' }
+  [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+  email: 'Not a valid email',
+  username: 'Must start withe a capital character!'
+};
