6eb132ed7d0ba201173e90d046a80cceeeabac2a
woojinfive <kh3996@gmail.com>
2025-01-11 16:56:08 -0400

    creating a generic function

--- max_typescript/dist/app.js
+++ max_typescript/dist/app.js
@@ -1,12 +1,18 @@
 "use strict";
-const names = []; // string[]
-// names[0].split(' ');
-const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('This is done!');
-    }, 2000);
-});
-promise.then(data => {
-    let splitedData = data.split(' ');
-    console.log(splitedData);
-});
+// const names: Array<string> = []; // string[]
+// // names[0].split(' ');
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('This is done!');
+//   }, 2000);
+// });
+// promise.then(data => {
+//   let splitedData = data.split(' ');
+//   console.log(splitedData);
+// });
+function merge(objA, objB) {
+    return Object.assign(Object.assign({}, objA), objB);
+}
+// console.log(merge({ name: 'AAA' }, { age: 30 }));
+const mergedObj = merge({ name: 'AAA', hobbies: ['Sports'] }, { age: 30 });
+console.log(mergedObj.age);
--- max_typescript/src/app.ts
+++ max_typescript/src/app.ts
@@ -1,13 +1,21 @@
-const names: Array<string> = []; // string[]
-// names[0].split(' ');
+// const names: Array<string> = []; // string[]
+// // names[0].split(' ');
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('This is done!');
-  }, 2000);
-});
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('This is done!');
+//   }, 2000);
+// });
 
-promise.then(data => {
-  let splitedData = data.split(' ');
-  console.log(splitedData);
-});
+// promise.then(data => {
+//   let splitedData = data.split(' ');
+//   console.log(splitedData);
+// });
+
+function merge<T, U>(objA: T, objB: U) {
+  return { ...objA, ...objB };
+}
+
+// console.log(merge({ name: 'AAA' }, { age: 30 }));
+const mergedObj = merge({ name: 'AAA', hobbies: ['Sports'] }, { age: 30 });
+console.log(mergedObj.age);
