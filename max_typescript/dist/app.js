70af82de48b1ea605fdb202b24c8df4511083496
woojinfive <kh3996@gmail.com>
2025-01-13 15:37:41 -0400

    decorator start

--- max_typescript/dist/app.js
+++ max_typescript/dist/app.js
@@ -1,68 +1,22 @@
 "use strict";
-// const names: Array<string> = []; // string[]
-// // names[0].split(' ');
-// const promise: Promise<string> = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     resolve('This is done!');
-//   }, 2000);
-// });
-// promise.then(data => {
-//   let splitedData = data.split(' ');
-//   console.log(splitedData);
-// });
-function merge(objA, objB) {
-    return Object.assign(Object.assign({}, objA), objB);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function Logger(constructor) {
+    console.log('Logging...');
+    console.log(constructor);
 }
-// console.log(merge({ name: 'AAA' }, { age: 30 }));
-const mergedObj = merge({ name: 'AAA', hobbies: ['Sports'] }, { age: 30 });
-console.log(mergedObj);
-function countAndDescribe(element) {
-    let descriptionText = 'Got no value.';
-    if (element.length === 1) {
-        descriptionText = 'Got 1 elements';
-    }
-    else if (element.length > 1) {
-        descriptionText = 'Got ' + element.length + ' elements.';
-    }
-    return [element, descriptionText];
-}
-console.log(countAndDescribe(['Sports', 'Cooking']));
-function extractAndConvert(obj, key) {
-    return 'Value: ' + obj[key];
-}
-console.log(extractAndConvert({ name: 'AAA' }, 'name'));
-class DataStorage {
+let Person = class Person {
     constructor() {
-        this.data = [];
+        this.name = 'AAA';
+        console.log('Creating person object...');
     }
-    addItem(item) {
-        this.data.push(item);
-    }
-    removeItem(item) {
-        if (this.data.indexOf(item) === -1) {
-            return;
-        }
-        this.data.splice(this.data.indexOf(item), 1);
-    }
-    getItems() {
-        return [...this.data];
-    }
-}
-const textStorage = new DataStorage();
-textStorage.addItem('AAA');
-textStorage.addItem('BBB');
-textStorage.removeItem('AAA');
-console.log(textStorage.getItems());
-const numberStorage = new DataStorage();
-numberStorage.addItem(1);
-console.log(numberStorage.getItems());
-function createCourseGoal(title, description, date) {
-    let courseGoal = {};
-    courseGoal.title = title;
-    courseGoal.description = description;
-    courseGoal.completeUntil = date;
-    return courseGoal;
-}
-const names = ['AAA', 'BBB'];
-// names.push('CCC');
-// names.pop();
+};
+Person = __decorate([
+    Logger
+], Person);
+const pers = new Person();
+console.log(pers);
