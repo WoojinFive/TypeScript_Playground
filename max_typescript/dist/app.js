b26b688c88b7294968e57f62a27a9739a8a12d5a
woojinfive <kh3996@gmail.com>
2025-01-19 15:16:48 -0400

    DOM element selection and OOP rendering

--- max_typescript/dist/app.js
+++ max_typescript/dist/app.js
@@ -1,1 +1,15 @@
 "use strict";
+class ProjectInput {
+    constructor() {
+        this.templateElement = document.getElementById('project-input');
+        // this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')!;
+        this.hostElement = document.getElementById('app');
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.formElement = importedNode.firstElementChild;
+        this.attach();
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.formElement);
+    }
+}
+const projectInput = new ProjectInput();
