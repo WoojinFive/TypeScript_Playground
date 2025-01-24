d673da7b219505d18f4a126e0c1d060c7f1b8683
woojinfive <kh3996@gmail.com>
2025-01-23 23:53:12 -0400

    rendering a map ith google maps

--- select_and_share_a_place/src/app.ts
+++ select_and_share_a_place/src/app.ts
@@ -3,11 +3,33 @@ import axios from 'axios';
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
-const GOOGLE_API_KEY = 'AIzaSyCB86i8gBsMzk6xUjEphNinO8fVH9ouXGo';
+const GOOGLE_API_KEY = '';
+
+type MapCoordinates = { lat: number; lng: number };
+// declare var google: any;
 
 type GoogleGeocodingResponse = {
   results: { geometry: { location: { lat: number, lng: number } } }[];
   status: 'OK' | 'ZERO_RESULS';
+}
+
+let map: google.maps.Map;
+async function initMap(coordinates: MapCoordinates): Promise<void> {
+  const { Map } = (await google.maps.importLibrary(
+    "maps"
+  )) as google.maps.MapsLibrary;
+  if (!map) {
+    map = new Map(document.getElementById("map") as HTMLElement, {
+      center: coordinates,
+      zoom: 16,
+    });
+  }
+  const marker = new google.maps.Marker({
+    position: coordinates,
+    map,
+  });
+  marker.setMap(map);
+  map.setCenter(coordinates);
 }
 
 function searchAddressHandler(event: Event) {
@@ -24,6 +46,7 @@ function searchAddressHandler(event: Event) {
         throw new Error('Could not fetch location!');
       }
       const coordinates = response.data.results[0].geometry.location;
+      initMap(coordinates);
     })
     .catch(err => {
       alert(err.message);
