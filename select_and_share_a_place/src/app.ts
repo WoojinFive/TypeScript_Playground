ce85bcec1cd41f87865df10b5e9af404c77a5a83
woojinfive <kh3996@gmail.com>
2025-01-23 23:13:34 -0400

    setting up a google API key

--- select_and_share_a_place/src/app.ts
+++ select_and_share_a_place/src/app.ts
@@ -1,6 +1,8 @@
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
+const GOOGLE_API_KEY = 'AIzaSyCB86i8gBsMzk6xUjEphNinO8fVH9ouXGo';
+
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
